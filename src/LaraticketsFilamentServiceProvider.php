cbde750a23ccc127e4b1ca9328af8dceb06038c6
Abdelkarim Mateos Sanchez <abdelkarim.mateos@castris.com>
2025-12-07 14:08:03 +0100

    chore: run pint code style fixes

--- src/LaraticketsFilamentServiceProvider.php
+++ src/LaraticketsFilamentServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace AichaDigital\LaraticketsFilament;
 
-use AichaDigital\LaraticketsFilament\Commands\LaraticketsFilamentCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
